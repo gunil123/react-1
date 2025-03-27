3258c62a223ce248671593f00adc2d54a9a00077
gunil123 <202130115@email.daelim.ac.kr>
2025-03-27 16:43:14 +0900

    update

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,13 +1,18 @@
 import MyB from "./MyButton"
 import { Button1, Button3 } from "./ButtonLib"
-
+import AboutPage from "./AboutPage"
+import Profile from "./Profile"
+import './App.css'
+  
 export default function App() {
   return (
-    <div>
+    <div className="wrapper">
     <h1>Hello React</h1>
     <MyB /><br />
     <Button1 />&nbsp;
     <Button3 />
+    <AboutPage />
+    <Profile />
     </div>
   )
 }
